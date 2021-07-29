ce62a9e76ca195f048c31d13ff5d46dc6bc4ab8f
VietDuc0908 <86755488+VietDuc0908@users.noreply.github.com>
2021-07-29 09:29:12 +0700

    Add project files.

---
+++ QLNS.Common/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNS.Common
+{
+    class Class1
+    {
+        //convert date time type
+        public static DateTime ToDateTime(string s);
+
+        //convert string to int
+
+        //convert datatable to list 
+        public static List<T> ConvertDataTable<T>(DataTable dt)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+
+        //convert number fomat
+
+        //convert 
+
+
+        //
+    }
+}
